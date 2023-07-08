1bc888b127a1ade479ba9b63aa98134115a5af22
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-08 00:28:34 -0400

    Import TodoApp to main.jsx using the path added before on vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import TodoApp from '@/components/TodoApp';
+
+const domContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(domContainer);
+root.render(<TodoApp />);
