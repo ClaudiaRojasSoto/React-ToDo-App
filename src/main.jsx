d17a40f607b15f3a10f70a25352a48c67d937c9b
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-08 21:49:34 -0400

    Wrape the root with React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -5,4 +5,8 @@ import TodoApp from '@/components/TodoApp';
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
-root.render(<TodoApp />);
+root.render(
+  <React.StrictMode>
+    <TodoApp />
+  </React.StrictMode>
+);
