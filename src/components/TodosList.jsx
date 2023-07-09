c6384f67572f69d1d305d8c4b0cfbc09d6088f12
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-08 21:52:37 -0400

    Create the TodoList.jsx file and their initial content

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,13 @@
+import TodoItem from '@/components/TodoItem';
+
+const TodosList = ({ todosProps }) => {
+    return (
+      <ul>
+        {todosProps.map((todo) => (
+            <TodoItem key={todo.id} itemProp={todo} />
+        ))}
+      </ul>
+    );
+  };
+  export default TodosList;
+  
