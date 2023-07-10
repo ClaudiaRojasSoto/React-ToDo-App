0da18c2601ce5c8533ad9004f41c77b2852a963b
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-10 14:51:24 -0400

    Add the rest of importations inside de logical of the app construction

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,13 +1,34 @@
-import Header from '@/components/Header';
-import TodosLogic from '@/components/TodosLogic';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '@/routes/Home';
+import About from '@/routes/About';
+import Login from '@/routes/Login';
+import Profile from '@/routes/Profile';
+import NotMatch from '@/routes/NotMatch';
+import Layout from '@/components/Layout';
+import SinglePage from '@/routes/SinglePage';
+import ProtectedRoute from '@/components/ProtectedRoute';
 
 const TodoApp = () => {
-    return (
-      <>
-        <Header />
-        <TodosLogic />
-      </>
-    );
-  };
-  export default TodoApp;
-  
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path=":slug" element={<SinglePage />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
+  );
+};
+export default TodoApp;
