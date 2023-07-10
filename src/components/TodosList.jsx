5d45986fb8e80d958de1bf9789f65c8ac0c946e6
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-10 14:52:57 -0400

    Add the code to this item

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,13 +1,13 @@
 import TodoItem from '@/components/TodoItem';
-
-const TodosList = ({ todosProps }) => {
-    return (
-      <ul>
-        {todosProps.map((todo) => (
-            <TodoItem key={todo.id} itemProp={todo} />
-        ))}
-      </ul>
-    );
-  };
-  export default TodosList;
-  
+import { useTodosContext } from '@/context/TodosContext';
+const TodosList = () => {
+  const { todos } = useTodosContext();
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} itemProp={todo} />
+      ))}
+    </ul>
+  );
+};
+export default TodosList;
