25c07eef217414529c68d010ee0fb0f771723c7c
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-10 14:49:26 -0400

    Add the logical inside the navbar and their items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,113 @@
-import { useState } from "react";
+import React, { useState, useRef, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '@/context/AuthContext';
+
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'about', text: 'About' },
+  { path: 'profile', text: 'Profile' },
+  { path: 'login', text: 'Login' },
+];
 
 const Navbar = () => {
-    const [dropdown, setDropdown] = useState(false);
-    useEffect(() => {
-        document.title = `Current state value: ${dropdown}`;
-      }, [dropdown]);
-    return (
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>
-          <button onClick={() => setDropdown((prev) => !prev)}>
-            Services <span>&#8595;</span>
-          </button>
-          {dropdown && (
-            <ul>
-              <li>Design</li>
-              <li>Development</li>
-            </ul>
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const { user, logout } = useAuthContext();
+
+  const ref = useRef();
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (navbarOpen && ref.current && !ref.current.contains(event.target)) {
+        setNavbarOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [navbarOpen]);
+
+  return (
+    <>
+      <nav ref={ref} className="navbar">
+        <button
+          className="toggle"
+          onClick={() => setNavbarOpen((prev) => !prev)}
+        >
+          {navbarOpen ? (
+            <MdClose style={{ width: '32px', height: '32px' }} />
+          ) : (
+            <FiMenu style={{ width: '32px', height: '32px' }} />
           )}
-          </li>
+        </button>
+        <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}>
+          {links.map((link) => {
+            let navLink = (
+              <NavLink
+                to={link.path}
+                onClick={() => setNavbarOpen(false)}
+              >
+                {link.text}
+              </NavLink>
+            );
+
+            let navItem = (
+              <li>
+                {navLink}
+              </li>
+            );
+
+            if (link.path === 'login') {
+              if (!user) {
+                navItem = (
+                  <li>
+                    {navLink}
+                  </li>
+                );
+              }
+            } else if (link.path === 'profile') {
+              if (user) {
+                navItem = (
+                  <li>
+                    {navLink}
+                  </li>
+                );
+              }
+            }
+
+            return (
+              <React.Fragment key={link.text}>
+                {navItem}
+              </React.Fragment>
+            );
+          })}
+          {!user && (
+            <li className="log-in">
+              <span>Log in to edit to-dos</span>
+            </li>
+          )}
         </ul>
       </nav>
-    );
-  };
-  export default Navbar;
-  
+
+      {user && (
+        <div className="logout">
+          <p>{user}</p>
+          {<button onClick={handleLogout}>Logout</button>}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
