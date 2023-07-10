db62cb93be82964274153131cbc3285fbb0b60bf
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-10 14:53:47 -0400

    Add the iport to Todos provider to the app logic

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,35 +1,14 @@
 import InputTodo from '@/components/InputTodo';
 import TodosList from '@/components/TodosList';
 
+import { TodosProvider } from '@/context/TodosContext';
+
 const TodosLogic = () => {
-    const todos = [
-        {
-          id: 1,
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: 2,
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ];
-    return (
-        <div>
-        <InputTodo />
-        <TodosList todosProps={todos} />
-        <ul>
-        {todos.map((todo) => (
-          <li>{todo.title}</li>
-        ))}
-      </ul>
-      </div>
-    )
-  }
-  export default TodosLogic;
-  
+  return (
+    <TodosProvider>
+      <InputTodo />
+      <TodosList />
+    </TodosProvider>
+  );
+};
+export default TodosLogic;
