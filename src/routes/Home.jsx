45d424f9143cf225ac44307ee4f78ad59a637c2a
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-10 14:40:16 -0400

    Create the routed folder on src folder

---
+++ src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header';
+import TodosLogic from '@/components/TodosLogic';
+
+const Home = () => {
+  return (
+    <div className="todos">
+      <Header>
+        <h1>todos</h1>
+        <p>Items will persist in the browser local storage</p>
+      </Header>
+      <TodosLogic />
+    </div>
+  );
+};
+export default Home;
