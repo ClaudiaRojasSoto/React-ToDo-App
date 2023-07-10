7f591233717ceaad115f30b7ba137777ed77b93b
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-10 14:54:27 -0400

    Add the browser route to the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import TodoApp from '@/components/TodoApp';
 
-const domContainer = document.getElementById("root");
+import '@/styles/app.css';
+
+const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <BrowserRouter>
+      <TodoApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
